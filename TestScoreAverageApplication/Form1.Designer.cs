cbfac307c072792779af26bf25662e5028df6c57
Kyle Zimmerman <kaz0010@auburn.com>
2022-03-13 09:58:17 -0700

    averageTestScoreApplication complete workig perfectly.

--- TestScoreAverageApplication/Form1.Designer.cs
+++ TestScoreAverageApplication/Form1.Designer.cs
@@ -28,13 +28,161 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.test1TextBox = new System.Windows.Forms.TextBox();
+            this.test2TextBox = new System.Windows.Forms.TextBox();
+            this.test3TextBox = new System.Windows.Forms.TextBox();
+            this.averageLabel = new System.Windows.Forms.TextBox();
+            this.calculateBtn = new System.Windows.Forms.Button();
+            this.clearBtn = new System.Windows.Forms.Button();
+            this.exitBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(97, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Three Test Scores";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(97, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Test Score #1";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(97, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Test Score #2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(97, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Test Score #3";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(110, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Average";
+            // 
+            // test1TextBox
+            // 
+            this.test1TextBox.Location = new System.Drawing.Point(185, 87);
+            this.test1TextBox.Name = "test1TextBox";
+            this.test1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.test1TextBox.TabIndex = 5;
+            // 
+            // test2TextBox
+            // 
+            this.test2TextBox.Location = new System.Drawing.Point(185, 119);
+            this.test2TextBox.Name = "test2TextBox";
+            this.test2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.test2TextBox.TabIndex = 6;
+            // 
+            // test3TextBox
+            // 
+            this.test3TextBox.Location = new System.Drawing.Point(185, 153);
+            this.test3TextBox.Name = "test3TextBox";
+            this.test3TextBox.Size = new System.Drawing.Size(100, 20);
+            this.test3TextBox.TabIndex = 7;
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.Location = new System.Drawing.Point(185, 192);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(100, 20);
+            this.averageLabel.TabIndex = 8;
+            // 
+            // calculateBtn
+            // 
+            this.calculateBtn.Location = new System.Drawing.Point(100, 235);
+            this.calculateBtn.Name = "calculateBtn";
+            this.calculateBtn.Size = new System.Drawing.Size(90, 52);
+            this.calculateBtn.TabIndex = 9;
+            this.calculateBtn.Text = "Calculate Average";
+            this.calculateBtn.UseVisualStyleBackColor = true;
+            this.calculateBtn.Click += new System.EventHandler(this.calculateBtn_Click);
+            // 
+            // clearBtn
+            // 
+            this.clearBtn.Location = new System.Drawing.Point(210, 235);
+            this.clearBtn.Name = "clearBtn";
+            this.clearBtn.Size = new System.Drawing.Size(75, 23);
+            this.clearBtn.TabIndex = 10;
+            this.clearBtn.Text = "Clear";
+            this.clearBtn.UseVisualStyleBackColor = true;
+            this.clearBtn.Click += new System.EventHandler(this.clearBtn_Click);
+            // 
+            // exitBtn
+            // 
+            this.exitBtn.Location = new System.Drawing.Point(210, 264);
+            this.exitBtn.Name = "exitBtn";
+            this.exitBtn.Size = new System.Drawing.Size(75, 23);
+            this.exitBtn.TabIndex = 11;
+            this.exitBtn.Text = "Exit";
+            this.exitBtn.UseVisualStyleBackColor = true;
+            this.exitBtn.Click += new System.EventHandler(this.exitBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(374, 329);
+            this.Controls.Add(this.exitBtn);
+            this.Controls.Add(this.clearBtn);
+            this.Controls.Add(this.calculateBtn);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.test3TextBox);
+            this.Controls.Add(this.test2TextBox);
+            this.Controls.Add(this.test1TextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Test Score Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox test1TextBox;
+        private System.Windows.Forms.TextBox test2TextBox;
+        private System.Windows.Forms.TextBox test3TextBox;
+        private System.Windows.Forms.TextBox averageLabel;
+        private System.Windows.Forms.Button calculateBtn;
+        private System.Windows.Forms.Button clearBtn;
+        private System.Windows.Forms.Button exitBtn;
     }
 }
 
--- TestScoreAverageApplication/Form1.cs
+++ TestScoreAverageApplication/Form1.cs
@@ -16,5 +16,49 @@ namespace TestScoreAverageApplication
         {
             InitializeComponent();
         }
+
+        private void calculateBtn_Click(object sender, EventArgs e)
+        {
+            const double HIGH_SCORE = 95.0; // High score value
+            double test1, test2, test3, average;
+
+            // Get the scores from the text boxes
+            test1 = double.Parse(test1TextBox.Text);
+            test2 = double.Parse(test2TextBox.Text);
+            test3 = double.Parse(test3TextBox.Text);
+
+            // calculate the average
+            average = (test1 + test2 + test3) / 3.0;
+
+            // display the average, rouded to 2 decimal places.
+            averageLabel.Text = average.ToString("n1");
+
+            // if the average is a high sore, congratulate
+            // the user with a message box.
+
+            if (average > HIGH_SCORE)
+            {
+                MessageBox.Show("Congratulations! Great job!");
+            }
+
+        }
+
+        private void clearBtn_Click(object sender, EventArgs e)
+        {
+            // clear all fields
+            test1TextBox.Text = "";
+            test2TextBox.Text = "";
+            test3TextBox.Text = "";
+            averageLabel.Text = "";
+
+            // reset the focus to test1
+            test1TextBox.Focus();
+        }
+
+        private void exitBtn_Click(object sender, EventArgs e)
+        {
+            // close program
+            this.Close();
+        }
     }
 }
